e032cd483f38b8a0d296728632dcc9d9c6d795b7
lbadocha87 <63849341+lbadocha87@users.noreply.github.com>
2022-03-14 20:22:16 +0100

    post post add

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,40 @@
 import "./Post.css";
+import axios from "axios";
+import { useState } from "react";
 
 const Post = (props) => {
-  console.log(props.post.likes);
+  const [likesCount, setLikesCount] = useState(props.post.likes.length);
+  const [doesUserLiked, setDoesUserLiked] = useState(
+    props.post.likes.filter((like) => like.username === props.user.username)
+      .length !== 0
+  );
+
+  const likePost = (id, isLiked) => {
+    const headers = {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: "Bearer " + (props.user ? props.user.jwt_token : ""),
+    };
+
+    axios
+      .post(
+        "http://akademia108.pl/api/social-app/post/" + (isLiked?'dislike':'like'),
+        {
+          post_id: id,
+        },
+        { headers: headers }
+      )
+      .then((req) => {
+        let reqData = req.data;
+        console.log(reqData);
+        setLikesCount(likesCount + 1);
+        setDoesUserLiked(!isLiked);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <div className="post">
       <div className="avatar">
@@ -14,14 +47,12 @@ const Post = (props) => {
         </div>
         <div className="postContent">{props.post.content}</div>
         <div className="likes">
-          <button className="btn">
-            {props.post.likes.filter(
-              (like) => like.username === props.user.username
-            ).length
+          <button className="btn" onClick={() => likePost(props.post.id, doesUserLiked)}>
+            {doesUserLiked
               ? "Dislike"
               : "Like"}
           </button>
-          {props.post.likes.length}
+          {likesCount}
         </div>
       </div>
     </div>
